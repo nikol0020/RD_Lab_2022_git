90375f8210ead9dcb072e4b7015711af0cd59b3b
Tetiana Chulkova <Tetiana.Chulkova@cantire.com>
2022-09-14 22:59:23 +0300

    HW_01: added checking the positive number

--- HW_01/prices.js
+++ HW_01/prices.js
@@ -1,16 +1,17 @@
 function Prices() {
     ApplePrice();
     OrangePrice();
+    return console.log('end task 1');
 }
 
 function ApplePrice(apple = 12.50, discount = 15) {
     do {
         apple = +prompt('ENTER THE COST OF APPLES', 12.50);
-    } while (!(isFinite(apple)));
+    } while (!(isFinite(apple) && apple > 0));
 
-    do {
-        discount = +prompt('WRITE DISCOUNT FOR APPLE PRICE VALUE', 15);
-    } while (!(isFinite(discount)));
+     do {
+         discount = +prompt('WRITE DISCOUNT FOR APPLES PRICE VALUE', 15);
+     } while (!(isFinite(discount) && discount > 0));
 
     let applePrice = (apple - apple * discount/100).toFixed(2);
     confirm(`Price of apples: ${applePrice}`);
@@ -20,11 +21,11 @@ function ApplePrice(apple = 12.50, discount = 15) {
 function OrangePrice(orange = 12.50, margin = 15) {
     do {
     orange = +prompt('ENTER THE COST OF ORANGE', 12.50);
-    } while (!(isFinite(orange)));
+    } while (!(isFinite(orange) && orange > 0));
 
     do {
-    margin = +prompt('WRITE MARGIN FOR ORANGE PRICE VALUE', 15);
-    } while (!(isFinite(margin)));
+        margin = +prompt('WRITE MARGIN FOR ORANGES PRICE VALUE', 15);
+    } while (!(isFinite(margin) && margin > 0));
 
     let orangePrice = (orange + orange * margin/100).toFixed(2);
     confirm(`Price of oranges: ${orangePrice}`);
@@ -32,11 +33,14 @@ function OrangePrice(orange = 12.50, margin = 15) {
 }
 
 function PriceTag() {
-    confirm('Task2');
+    confirm('T a s k 2');
     let price = ApplePrice();
-    let message = `Apple from Ukraine. \nPrice: ${price}hrn`;
+    let message = `Apples from Ukraine. \nPrice: ${price}hrn`;
+    let messageSecondWay = `Apples from Ukraine. 
+    Price: ${price}hrn`;
     confirm(message);
-    document.write(`Apple from Ukraine. Price: ${price}hrn`);
+    confirm(messageSecondWay);
+    document.write(`Apples from Ukraine. Price: ${price}hrn`);
     return price;
 }
 
